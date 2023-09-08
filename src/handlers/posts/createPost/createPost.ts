6c43dba51e002e495496226fe2528bf3276b20d6
sai madhan <saimadhan.k@agiratech.com>
2023-09-08 19:14:54 +0530

    creates and retreives posts

--- src/handlers/posts/createPost/createPost.ts
+++ src/handlers/posts/createPost/createPost.ts
@@ -1,5 +1,29 @@
 import { Request, Response } from "express";
+import Post from "../../../data/models/posts";
+import { logger } from "../../../utils/logger";
 
-function createPost(req: Request, res: Response) {}
+async function createPost(req: Request, res: Response) {
+  const { title, imageUri, tags } = req.body;
+
+  const { id } = req.user;
+
+  try {
+    const newPost = new Post({
+      title: title,
+      imageUri: imageUri,
+      tags: tags,
+      createdBy: id,
+    });
+
+    const post = await newPost.save();
+
+    logger.info({ action: "New Post Created", post });
+    res.status(200).json({ message: "New Post Created", post });
+  } catch (error) {
+    logger.error({ action: "Create New Post", error });
+
+    res.status(500).json({ message: "internal Server Error" });
+  }
+}
 
 export default createPost;
